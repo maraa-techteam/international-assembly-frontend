6e76901240f1a6e091c62ed4e84cc40d34367491
Aleksei Belozertcev <siteintassaml@gmail.com>
2026-02-23 20:08:12 +0100

    perf: cache enabled

--- src/common/lib/directus.ts
+++ src/common/lib/directus.ts
@@ -1,14 +1,14 @@
 import { createDirectus, rest } from '@directus/sdk'
 
+const directusCmsUrl = `https://${process.env.DIRECTUS_CMS_URL}`
+
 if (!process.env.DIRECTUS_CMS_URL) {
   throw new Error('DIRECTUS_CMS_URL environment variable is not defined')
 }
 
-const directusCmsUrl = `https://${process.env.DIRECTUS_CMS_URL}`
-
 const directus = createDirectus(directusCmsUrl).with(
   rest({
-    onRequest: (options) => ({ ...options, cache: 'no-store' }),
+    onRequest: (options) => ({ ...options }),
   }),
 )
 
