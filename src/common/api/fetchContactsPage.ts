88836976cc91ed59e125d0adf5962a98b152e628
Aleksei Belozertcev <siteintassaml@gmail.com>
2026-03-18 18:24:44 +0100

    feat: create contacts page

---
+++ src/common/api/fetchContactsPage.ts
@@ -0,0 +1,25 @@
+import { readItems } from '@directus/sdk'
+
+import directus from '../lib/directus'
+
+export async function fetchContactsPage() {
+  try {
+    const raw = await directus.request(
+      readItems('contacts_page', {
+        fields: ['meta_title', 'meta_description', 'title', 'text'],
+      }),
+    )
+    return raw.map((item) => {
+      return {
+        meta_title: item.meta_title,
+        meta_description: item.meta_description,
+        title: item.title,
+        text: item.text,
+      }
+    })
+  } catch (error) {
+    throw new Error(
+      `Failed to fetch contacts page data: ${error instanceof Error ? error.message : String(error)}`,
+    )
+  }
+}
